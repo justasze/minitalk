61ed1e14fd4b42b57de004645bc47a4cbfebf3c4
Ashbury <justasze@student.42.fr>
2018-03-31 16:07:15 +0200

    Even greater refinement

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: justasze <justasze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/31 11:41:43 by bcozic            #+#    #+#             */
-/*   Updated: 2018/03/31 16:00:11 by justasze         ###   ########.fr       */
+/*   Updated: 2018/03/31 16:06:11 by justasze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,36 @@
 #include <stdlib.h>
 #include "libft.h"
 
-static void	transmit_bits(int *bit_shift, int pid, char *msg)
+static void	transmit_end_of_string(int pid)
+{
+	int	bit_shift;
+
+	bit_shift = -1;
+	while (++bit_shift < 8)
+	{
+		if (kill(pid, SIGUSR2) == -1)
+			exit(1);
+		usleep(100);
+	}
+}
+
+/*
+** Here, we send the message bit by bit, with SIGUSR1 for 1
+** and SIGUSR2 for 0
+*/
+
+static void	transmit_message(int pid, char *msg)
 {
 	int	i;
+	int	bit_shift;
 
 	i = 0;
 	while (msg[i])
 	{
-		*bit_shift = -1;
-		while (++(*bit_shift) < 8)
+		bit_shift = -1;
+		while (++bit_shift < 8)
 		{
-			if ((msg[i] & (0x80 >> *bit_shift)) == 0)
+			if ((msg[i] & (0x80 >> bit_shift)) == 0)
 			{
 				if (kill(pid, SIGUSR2) == -1)
 					exit(1);
@@ -36,23 +55,16 @@ static void	transmit_bits(int *bit_shift, int pid, char *msg)
 		}
 		i++;
 	}
+	transmit_end_of_string(pid);
 }
 
 int			main(int ac, char **av)
 {
-	int	bit_shift;
 	int	pid;
 
 	if (ac != 3)
 		return (0);
 	pid = ft_atoi(av[1]);
-	transmit_bits(&bit_shift, pid, av[2]);
-	bit_shift = -1;
-	while (++bit_shift < 8)
-	{
-		if (kill(pid, SIGUSR2) == -1)
-			return (1);
-		usleep(100);
-	}
+	transmit_message(pid, av[2]);
 	return (0);
 }
